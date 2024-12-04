7742fbb8d4df035c6702a8a895a99b8821aeb31f
Alhassan <alhassanfedawy620@gmail.com>
2024-12-04 15:00:31 +0200

    deploy issue

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
+}
+
+module.exports = nextConfig
